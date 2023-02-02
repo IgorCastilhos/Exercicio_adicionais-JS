e3ad7cded824b5b495b770a18293a37b948897fd
Igor Castilhos <igorcastilhos2010@hotmail.com>
2023-02-01 23:49:01 -0300

    Adding Exercicio 9

--- 8.ExercitandoFuncoes/script.js
+++ 8.ExercitandoFuncoes/script.js
@@ -2,36 +2,46 @@ const spaceshipName = prompt(`Digite o nome da nave: `);
 let velocity = 0;
 let option = "";
 
-do {
-  option = prompt(`
-  Bem-vindo(a)! Escolha uma opção:
-  1. Acelerar a nave em 5km/s 
-  2. Desacelerar em 5km/s
-  3. Imprimir dados de bordo
-  4. Sair
-    `);
-  switch (option) {
-    case "1":
-      velocity += 5;
-      break;
+function showMenu() {
+  do {
+    option = prompt(`
+    Bem-vindo(a)! Escolha uma opção:
+    1. Acelerar a nave em 5km/s 
+    2. Desacelerar em 5km/s
+    3. Imprimir dados de bordo
+    4. Sair
+      `);
+    switch (option) {
+      case "1":
+        accelerate();
+        break;
+      case "2":
+        desacelerate();
+        break;
+      case "3":
+        printData();
+        break;
+      case "4":
+        alert(`Saindo...`);
+        break;
+      default:
+        alert(`Opção Inválida!`);
+        break;
+    }
+  } while (option !== "4");
+}
 
-    case "2":
-      velocity -= 5;
-      break;
+function accelerate() {
+  velocity += 5;
+}
 
-    case "3":
-      alert(`Nome da nave: ${spaceshipName}
-        Velocidade da nave: ${velocity}`);
-      break;
+function desacelerate() {
+  velocity -= 5;
+}
 
-    case "4":
-      break;
+function printData() {
+  alert(`Nome da nave: ${spaceshipName}
+  Velocidade da nave: ${velocity}`);
+}
 
-    default:
-      alert(`Opção Inválida!`);
-      break;
-  }
-} while (option !== "4");
-
-function showMenu() {}
-function printData() {}
+showMenu();
